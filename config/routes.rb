0dac6277fb7917aa544f1b00916e74aa7cd2b0ea
Leeten Chin <lchin@imsystems.com.au>
2021-06-12 16:31:26 +1000

    Saturday coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
+  # get "tasks" => "tasks#index"
+  # # the above is the same as  get "/tasks", to: "tasks#index"
+
+  # # new form then save
+  # get "tasks/new" => "tasks#new", as: :new_task
+  # post "tasks" => "tasks#create"
+
+  # get "tasks/:id" => "tasks#show", as: :task
+
+  # # get the task, edit, update
+  # get "tasks/:id/edit" => "tasks#edit", as: :edit_task
+  # patch "tasks/:id" => "tasks#update"
+
+  # # delete
+  # delete "tasks/:id" => "tasks#destroy"
+
+  resources :tasks
+
 end
